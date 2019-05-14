ada5ebfa7b7b52c03397f95cff35b3700184d39c
LabPC <q30104@gmail.com>
2019-05-14 18:22:11 +0800

    Strtegy Done

--- src/strategy/scripts/calculate.py
+++ src/strategy/scripts/calculate.py
@@ -1,34 +1,37 @@
-
 import rospy
 from transfer.msg import PPoint
 # Normalization
 HALF_MAX_DIS = 1500 #cm
 FULL_MAX_DIS = 2163 #cm
 
-input_std_list = []
+state = []
 
-def input2std_list(data):
+def input2state(data):
     input_list = [data.rival_An, data.rival_Dis, data.right_angle, data.right_radius, data.border_An, data.border_Dis]
     print(all_input_list)
-    global input_std_list
-    input_std_list =  [data.rival_An/180, data.rival_Dis/HALF_MAX_DIS, data.right_angle/180, data.right_radius/HALF_MAX_DIS, data.border_An/180, data.border_Dis/HALF_MAX_DIS]
-
+    global state
+    state =  [data.rival_An/180, data.rival_Dis/HALF_MAX_DIS, data.right_angle/180, data.right_radius/HALF_MAX_DIS, data.border_An/180, data.border_Dis/HALF_MAX_DIS]
 
 def get_input():
-
     rospy.init_node('input', anonymous=True)
-    rospy.Subscriber("/nubot1/omnivision/OmniVisionInfo/GoalInfo", PPoint, input2std_list)
+    rospy.Subscriber("/nubot1/omnivision/OmniVisionInfo/GoalInfo", PPoint, input2state)
     rospy.spin()
+
+# def action():
+def action2output(action):
+    kickpwr 
+
 
 if __name__ == '__main__':
     get_input()
 
 
 '''calculate'''
-class Calculate():
+class sac_cal():
     def __init__():
 
     def input(self):
+        
         
     def output(self):
 
